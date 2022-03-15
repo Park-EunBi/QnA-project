ccd4d302ef3630dd3b405c51c1ebfb8b6010bcd8
Soojin-Lee-01 <sojinlee1004@naver.com>
2022-03-16 02:30:26 +0900

    feature/open_room-soojin

---
+++ openroom/app/src/main/java/com/soojin/a2022_qna/board/BoardListview_Adapter.kt
@@ -0,0 +1,24 @@
+package com.soojin.a2022_qna.board
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class BoardListview_Adapter(val boardList : MutableList<BoardModel>) : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return 10
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+}
